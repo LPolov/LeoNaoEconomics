25d889e8fa3608356bf2e8e3b9163ac6fd9460df
Leontii Polovinko <half.leon11@gmail.com>
2023-05-17 22:23:36 +0300

    Initial commit

---
+++ src/main/java/com/polovinko/leontii/LeoNaoEconomics/LeoNaoEconomicsApplication.java
@@ -0,0 +1,13 @@
+package com.polovinko.leontii.LeoNaoEconomics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoNaoEconomicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoNaoEconomicsApplication.class, args);
+	}
+
+}
